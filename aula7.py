4b1082cb0dd48941de4c5b2a742b46c342e70d5f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 17:03:15 -0300

    Create aula7.py

---
+++ aula7.py
@@ -0,0 +1,17 @@
+# variaveis são usaddas para salvar algo na memória # # do computador.
+# PEP8: inicie varáveis com letras minúsculas, pode usar 
+# números e underline _.
+# O sinal de = é o operador de atribuição . ele é # # # usado para   atribuir um valor a um nome (variável).
+# USO: nome_variável = expressão
+
+#nome_completo = 'sivaldo vieira'
+#soma_dois_mais_dois = 2 + 2
+#int_um = bool('1')
+#print(int_um, type(int_um))
+#print(nome_completo, soma_dois_mais_dois)# sivaldo vieira
+
+nome = 'sivaldo'
+idade = 33
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior ?', maior_de_idade)
