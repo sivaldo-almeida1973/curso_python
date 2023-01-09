50a5b484ee13ab819eb8f184c38c2f8b3f38c371
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 17:51:03 -0300

    Create aula10.py

---
+++ aula10.py
@@ -0,0 +1,11 @@
+concatenação =  'A' + 'B' + 'C'
+print(concatenação)
+
+concatenação_2 = 'sivaldo' + ' ' + 'vieira'
+print(concatenação_2)
+
+
+a_dez_vezes = 'A' * 10
+tres_vezes_lucas = 3 * 'lucas'
+print(a_dez_vezes)
+print(tres_vezes_lucas)
