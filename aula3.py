786af74b5f8a1a767e7b267adde0270e3f58bca8
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 15:40:00 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,24 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica / Forte
+str -> strings -> texto
+Strings são textos que estão dentro de aspas
+
+"""
+print(1234)#numero inteiro
+# Aspas simples
+print('sivaldo vieira')#string ('')
+
+
+# Aspas duplas   
+print("Sivaldo vieira")#string ("")
+print(1 ,'Sivaldo "vieira" ')#string com escape ("")
+
+
+
+# Escape
+print("Sivaldo \"vieira\"")#string com escape coloca aspas na palavra ("")
+print('Sivaldo "vieira"')#string com escape ("")
+
+# r
+print(r"Sivaldo \"vieira\"")#string com escape coloca (\) na palavra .
