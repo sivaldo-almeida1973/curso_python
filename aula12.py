a6679b583daf1033e106a584feedac8a3655215d
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 19:17:24 -0300

    Create aula12.py

---
+++ aula12.py
@@ -0,0 +1,18 @@
+nome = 'Lucas Almeida'
+altura = 1.75
+peso = 79
+# IMC = peso / (altura + altura)
+imc = peso / altura ** 2
+# (...) placeroder
+
+print('Nome:',nome , 'Altura:',altura , 'Peso:',peso, 'IMC:' , imc)
+
+# ou
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+#Lucas Almeida tem 1.75 de altura,
+#pesa 79 quilos e seu IMC é
+# 29.32
