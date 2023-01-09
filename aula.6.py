9fb72fe99f85351c3af2d2d10b64643ecaf7469a
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 16:37:55 -0300

    Create aula.6.py

---
+++ aula.6.py
@@ -0,0 +1,15 @@
+# conversão de tipos , coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro 
+# tipos imutaveis e primitivos:
+# str, int, float, bool
+
+print(int('1'), type(int('1'))) # coversão para int
+
+
+print(type(float('1') + 1)) # coversão para float
+
+print(bool(''))# coversão para bool
+print(bool(' '))
+
+print(str(11)+ 'b')# coversão parastr
