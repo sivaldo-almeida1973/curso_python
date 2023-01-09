7110b940c43364d002ff1dc0788acdab0d94fea0
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 15:39:00 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,6 @@
+
+# \r\n  QUEBRA DE LINHAS automatico do windows (CRLF)
+print(12, 34)#argumentos não nomeados
+#separadores de linhas \r\n 
+print(12, 34 ,121, sep='-', end='\n#')
+print(12, 34, sep='-' , end='\n')
