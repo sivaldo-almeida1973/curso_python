995f329cd1df9ef436e1bfbca32cdd32c2a270bc
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 15:42:29 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,22 @@
+# Tipos (Int) e (Float)
+# int -> Números inteiros
+# O tipo int representa qualquer nùmero
+# Positivo ou negativo. int sem sinal é considerado.
+print(11) #int#
+print(-11) #int
+print(0) #int
+
+# Float -> Nùmero com ponto flutuante
+# o tipo Float representa qualquer número
+# positivo ou Negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+
+print(1.1, 10.11) #float
+print(0.0) #float
+print(0.0, -1.2) #float
+
+# A função type mostra o tipo que o Python inferiu ao valor.
+
+print(  type('sivaldo')   )
+print(  type(3)   )
+print(  type(1.1) ,type(-1.2)  )
