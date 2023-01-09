8fcaf10b8a7d4ab4492abad1822f646b0500eacb
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-09 17:38:54 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,31 @@
+adição =  10 + 10
+print('Adição', adição)
+
+subtração =  10 - 5
+print('Subtração', subtração)
+
+multiplicação = 10 * 10
+print('Multiplicação', multiplicação)
+
+divisão = 10 / 2.2  #resultado sempre é float
+print('Divisão', divisão)
+
+divisão_inteira = 10 // 2.2
+print('Divisão inteira', divisão_inteira)
+
+exponenciação = 2 ** 10
+print('Exponenciação', exponenciação)
+
+modulo = 55 % 2 # resto da divisão
+print('Modulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
+
+
+
+
+
