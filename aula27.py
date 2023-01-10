e93564d6638a62d3d50f536194e8cd728aa94686
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-10 19:38:59 -0300

    Create aula27.py

---
+++ aula27.py
@@ -0,0 +1,14 @@
+"""
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de carateres da str,
+"""
+
+variavel = 'Olá mundo'
+print(variavel [:])
+print(len(variavel))
+print(variavel[0:9:4])
