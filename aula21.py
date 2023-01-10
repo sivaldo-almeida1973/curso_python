5c69883b5413d5c320f1e473b7fbca57b20bd50c
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-10 15:51:31 -0300

    Create aula21.py

---
+++ aula21.py
@@ -0,0 +1,26 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições pescisam ser 
+# verdadeiras.
+
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São consideradas falsy (que vc já)
+# (0 0.0 '' False)
+# Tambem existe o tipo (None) que é
+# usado para representar um (não valor)
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+
+senha_permitida = '123456'
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
+
+# Avaliação de curto cicuito
+
+#print(True and False and True)
+#print(bool(0))
