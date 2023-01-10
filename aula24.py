0e0ee26c4a2afff06d28985fbc1d3c279eb8506a
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-10 17:47:52 -0300

    Create aula24.py

---
+++ aula24.py
@@ -0,0 +1,22 @@
+# Oeradores in e not in
+# strings são iteráveis
+# 0 1 2 3 4 5
+# o t á v i O
+# -6-5-4-3-2-1
+#nome = "sivaldo"
+#print(nome[3])
+#print(nome[-3])
+
+#print("a" in nome)
+#print('c' in nome)
+#print(10 * '-')
+#print('d' not in nome)
+#print('f' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome} ')
+else:
+    prin(f'{encontrar} não está em {nome}' )
