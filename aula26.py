e3bf0356830c402b9f02a6f879047d2974485a55
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-10 18:56:01 -0300

    Create aula26.py

---
+++ aula26.py
@@ -0,0 +1,24 @@
+"""
+Formatação básica de strings
+s - strings
+d - int
+f - float
+.<numero de digitos>f
+x ou X - Hexadecimal
+(caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o numero a aparecer antes dos zeros
+Sinal - + ou -
+ex.: 0>-100, .1f
+Conversion flags - !r !s !a
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel:$^10}.')
+print(f'{1000.000044556:0=+10,.1f}')
+print(f'o hexadecimal de 1500 é {1500:08X} ')
