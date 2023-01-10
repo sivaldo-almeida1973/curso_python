1f0fa2f545d17e1c0d9c59549f4191640589cd48
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-10 18:12:06 -0300

    Create aula25.py

---
+++ aula25.py
@@ -0,0 +1,23 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+
+"""
+
+nome = 'Lucas'
+preço = 1000.95897643
+variavel = '%s, o preço é RS%.2f' % (nome, preço)
+print(variavel)
+
+
+
+
+
+
+
+
+
+print('O hexadecimal de %d é %2X' % (150, 150))
