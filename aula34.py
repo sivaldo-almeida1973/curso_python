96632d9f19e7f1598fa86f46d1b7740f767797a4
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-11 19:31:31 -0300

    Create aula34.py

---
+++ aula34.py
@@ -0,0 +1,14 @@
+"""
+Repetições
+while (enquanto)
+executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> quando um codigo não tem fim
+"""
+condicao = True
+
+
+while condicao:
+    nome = input('qual o seu nome: ')
+    print(f'seu nome é{nome}')
+    
+
