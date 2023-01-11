4ccc4a03f5cc0bfefd1cff27d257940fea2c702f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-11 07:28:52 -0300

    Update aula27.py

--- aula27.py
+++ aula27.py
@@ -9,6 +9,6 @@ de carateres da str,
 """
 
 variavel = 'Olá mundo'
-print(variavel [8:9])
-print(len(variavel))
-print(variavel[0:9:4])
+print(variavel [:9])# toda variavel
+print(len(variavel))# quantas letras
+print(variavel[-1])#ultima letra
