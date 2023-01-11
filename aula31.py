957de699116cfce754c09ad08bc620f486df7ed7
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-11 11:20:08 -0300

    Create aula31.py

---
+++ aula31.py
@@ -0,0 +1,24 @@
+'''
+flag (Bandeira) - marcar local
+None = Não valor
+is e is nott = é ou não é (tipo , valor, identidade)
+id = identidade
+
+'''
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('faça algo')
+else:
+   
+    print('não faça nada')
+
+
+if passou_no_if is None:
+    print('não passou no if')
+
+else:
+    print('passou no if')
