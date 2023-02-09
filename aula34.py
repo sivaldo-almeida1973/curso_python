87b30ef515d3489c69056fcc7abc4dcb9052e101
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-09 18:18:00 -0300

    Update aula34.py

--- aula34.py
+++ aula34.py
@@ -4,11 +4,16 @@ while (enquanto)
 executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> quando um codigo não tem fim
 """
-condicao = True
+condicao = False
 
 
 while condicao:
     nome = input('qual o seu nome: ')
-    print(f'seu nome é{nome}')
+    print(f'seu nome é :  { nome}  ')
+
+    if nome == 'sair':
+        break
+
+print('acabou')
     
 
