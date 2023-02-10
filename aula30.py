12b0a2f6ce6fa2eb360d72e8948353d21574305b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 20:33:32 -0300

    Update aula30.py

--- aula30.py
+++ aula30.py
@@ -1,10 +1,10 @@
 '''
-CONSTANTE = 'variáveis' que vão mudar
-Muitas condições no mesmo if (ruim)
+CONSTANTE = 'variáveis' que não vão mudar
+Muitas condições no mesmo if é (ruim)
 ....<- Contagem de complexidade (ruim)
 '''
-velocidade = 65 # velocidadee atual do carro
-local_carro = 100 # local em que ocarro estaá na estrada
+velocidade = 60 # velocidade atual do carro
+local_carro = 100 # local em que ocarro está na estrada
 
 RADAR_1 = 60 # velocidade máxima do radar 1
 LOCAL_1 = 100 # local onde o radar 1 esta
@@ -17,7 +17,7 @@ carro_passou_radar_1 =  local_carro >= (LOCAL_1 - RADAR_RANGE) and \
 carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
 
 if velocidade > RADAR_1:
-    print('Velocidade carro ultrapassou do radar 1')
+    print('Velocidade carro ultrapassou no radar 1')
 
 if carro_passou_radar_1:
     print('Carro passou radar 1')
@@ -25,5 +25,8 @@ if carro_passou_radar_1:
 if carro_multado_radar_1:
     print('carro multado em radar 1')
 
+else:
+    print('carro abaixo da velocidade do radar 1')
+print('dirija com cuidado!!')
 
 
