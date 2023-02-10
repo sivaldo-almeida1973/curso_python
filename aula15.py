74852a20599227137142ab69fc36a7ff393f0e99
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 11:25:33 -0300

    Update aula15.py

--- aula15.py
+++ aula15.py
@@ -1,7 +1,16 @@
 #nome = input("Qual o seu nome? ")
 #print(f'o seu nome é {nome=}')
 
+#print(f'A soma de {numero1} + {numero2} é {soma=}:')
+
+#aqui gerou uma concatenação 
+
+print('-')
+#para vitar o erro é melhor dessa forma
 numero1 = int(input('Digite um numero:'))
 numero2 = int(input('Digite outro numero:'))
-soma = numero1 + numero2
-print(f'A soma de {numero1} + {numero2} é {soma=}:')
+
+int_numero1 = int(numero1)
+int_numero2 = int(numero2)
+soma= int_numero1 + int_numero2
+print(f'A soma de {int_numero1} + {int_numero2}  e igual a {soma}:')
