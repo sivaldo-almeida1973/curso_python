86eafd87f2badebf9efc372d69d9a78f932ffb80
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 11:25:24 -0300

    Update aula13.py

--- aula13.py
+++ aula13.py
@@ -6,11 +6,11 @@ imc = peso / altura ** 2
 
 # f-strings (formatação)
 
-linha_1 = f'{nome} tem {altura:.2f} de altura'
-linha_2 = f'pesa {peso} quilos e seu imc é'
-linha_3 = f'{imc:.2f} '
+linha_1 = (f'{nome} tem {altura:.2f} de altura')
+linha_2 =( f'pesa {peso} quilos e seu imc é {imc:.2f}')
+#linha_3 = f'{imc:.2f} '
 
 
 print(linha_1)
 print(linha_2)
-print(linha_3)
+#print(linha_3)
