9619a185dd33e800a63b8d87ccd7fccfb1849c39
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 11:25:49 -0300

    Create aula36.py

---
+++ aula36.py
@@ -0,0 +1,23 @@
+#Operadores de Atribuição com operadores Aritimeticos
+
+# = += -= /= //= **= %=
+
+
+#contador = 0
+
+####
+#contador = contador + 1
+#print(contador)
+
+#Igual a:
+
+#contador += 1
+#print(contador)
+
+##mesma coisa acontece com string
+contador = 10
+
+
+####
+contador *= 2
+print(contador)
