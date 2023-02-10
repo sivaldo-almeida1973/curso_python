7e35e98148fdbc397c042af2261512babf6cd685
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 20:33:26 -0300

    Update aula29.py

--- aula29.py
+++ aula29.py
@@ -3,9 +3,7 @@ Introdução ao tr/except
 except -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 '''
-numero_str = input(
-    'Vou dobrar o número que você digitar: '
-    )
+numero_str = input('Vou dobrar o número que você digitar: ')
 try:
     numero_float = float(numero_str)
     print('FLOAT:', numero_float)
@@ -20,3 +18,5 @@ except:
 #    print(f' o dobro de {numero_str} é {numero_float# * #2:.2f} ')
 #else:
 #   print('isso não é um número')
+
+print(numero_str.isdigit())
