157d54fcfe417655c07b66971b904ad1a31655ef
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 20:33:23 -0300

    Update aula27.py

--- aula27.py
+++ aula27.py
@@ -3,12 +3,15 @@ Fatiamento de strings
 012345678
 Olá mundo
 -987654321
-Fatiamento [i:f:p] [::]
+Fatiamento [i:f:p] [::] _____i(indice) f(fim)  p(passos)
 Obs.: a função len retorna a qtd
 de carateres da str,
 """
 
 variavel = 'Olá mundo'
-print(variavel [:9])# toda variavel
+print(variavel [4:])# toda variavel
+print(variavel [0:9:2])# do 0 ate o 9 de 2 em 2
+print(variavel [::-1])# do 0 ate o 9 de invertido
+print(variavel [-1:-10:-1])# do 0 ate o 9 de invertido
 print(len(variavel))# quantas letras
 print(variavel[-1])#ultima letra
