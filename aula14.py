36a9d6079e6fbdc35db785d9a36c027704e45d0a
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 11:25:30 -0300

    Update aula14.py

--- aula14.py
+++ aula14.py
@@ -1,10 +1,14 @@
 a = 'A'
 b = 'B'
 c = 1.1
-string = 'a={0} b{1} c={2:.2f}'
+string = ('a={0} b={1} c={2:.2f}')
 formato = string . format(a, b, c) # argumento
+print(formato)
 
 
-string = 'a={nome1} b{nome2} c={nome3:.2f}'
-formato = string . format(nome1=a, nome2=b,nome3=c)#parametro nomeado
+a = '10'
+b = '20'
+c = 1.1
+string = ('a={nome1} b{nome2} c={nome3:.2f}')
+formato = (string . format(nome1=a, nome2=b,nome3=c))#parametro nomeado
 print(formato)
