55a40da96849226abd0ad8af33fcc3a66dc3cf3f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-10 11:25:40 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -4,3 +4,9 @@ print(12, 34)#argumentos não nomeados
 #separadores de linhas \r\n 
 print(12, 34 ,121, sep='-', end='\n#')
 print(12, 34, sep='-' , end='\n')
+
+print(12,34, sep='-')
+print(2,3,4,5,6,7, sep='')
+print(2,3,4,5,6,7, '\n')
+print(2,3,4,5,6,7, '\n')
+print(2,3,4,5,6,7, sep='##')
