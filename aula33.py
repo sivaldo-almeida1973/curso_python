8ba63645516679fd094c186b89709a9ef27bfbf2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-11 18:22:11 -0300

    Update aula33.py

--- aula33.py
+++ aula33.py
@@ -6,8 +6,9 @@ imutaveis que vimos: str, int, float, bool(são objetos)
 """
 
 strings = "sivaldo vieira"
-outra_variavel = f'{strings[:3]}ABC{strings[4:]}'
+outra_variavel = ( f'{strings[:3]}ABC{strings[4:]}')
 print(strings)
 print(outra_variavel)
-print(strings.capitalize())
+print(strings.capitalize())#primeira letra vem maiuscula
+print(strings.zfill(20))
 
