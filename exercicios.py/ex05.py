48154dacb2eab7e630f37716b74046b16279ba87
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-11 18:22:43 -0300

    Create ex05.py

---
+++ exercicios.py/ex05.py
@@ -0,0 +1,11 @@
+"""interando strings com while
+"""
+#indices 01234567891011121314
+nome = 'sivaldo vieira'
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[4])
+
+nova_string += '*s*'
