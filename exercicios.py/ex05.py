941e6a4fb5c622edccac1444460b66b4cb647429
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-12 17:19:22 -0300

    Update ex05.py

--- exercicios.py/ex05.py
+++ exercicios.py/ex05.py
@@ -3,9 +3,15 @@
 #indices 01234567891011121314
 nome = 'sivaldo vieira'
 
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
-print(nome[4])
-
-nova_string += '*s*'
+#tamanho_nome = len(nome)
+#print(nome)#
+#print(tamanho_nome)
+#print(nome[4])
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+  letra = nome[indice]
+  novo_nome += f'*{letra}'
+  indice += 1
+novo_nome += '*'
+print(novo_nome)
