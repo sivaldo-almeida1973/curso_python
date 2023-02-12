7af71b8f50211dafd2e7b2e155034582a50e067f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-12 17:52:44 -0300

    Update aula39.py

--- aula39.py
+++ aula39.py
@@ -8,13 +8,17 @@ while True:
   operador = input('Digite o operador (+-/*): ')
 
   numeros_validos = None
+  num_1_float = 0
+  num_2_float = 0
+  
 
   try:
-    num_1_float = float(numero_1)
-    num_2_float = float(numero_2)
+      num_1_float = float(numero_1)
+      num_2_float = float(numero_2)
+      numeros_validos = True
 
   except:
-    numeros_validos = None
+      numeros_validos = None
 
 
   if numeros_validos is None:
@@ -30,10 +34,22 @@ while True:
   if len(operador) > 1:
     print('Digite apenas um operador')
     continue
+  
+  print('Realizando sua conta.Confira o resultado abaixo.')
+  if operador == ' + ':
+    print(f'{num_1_float } + { num_2_float } = ' , num_1_float + num_2_float )
+  elif operador == '-':
+     print(f'{num_1_float } - { num_2_float } = ' , num_1_float - num_2_float )
+  elif operador == '/':
+     print(f'{num_1_float } / { num_2_float } = ' , num_1_float / num_2_float )
+  elif operador == '*':
+     print(f'{num_1_float } * { num_2_float } = ' , num_1_float * num_2_float )
+  else:
+    print('nunca deveria chegar aqui.')
 
   sair = input('Quer sair?  [s]im : ').lower().startswith('s')
   
   if sair is True:
     break
 
-  print(sair)
+
