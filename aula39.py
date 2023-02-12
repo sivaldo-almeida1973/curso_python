1f0a9027b57e2a566546e774389e81742de0818f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-12 17:19:19 -0300

    Create aula39.py

---
+++ aula39.py
@@ -0,0 +1,39 @@
+""""calculadora com while
+lower()# converte em minuscula
+startswith('s')#começa com (s)
+"""
+while True:
+  numero_1 = input('digite um numero: ')
+  numero_2 = input('digite outro numero: ')
+  operador = input('Digite o operador (+-/*): ')
+
+  numeros_validos = None
+
+  try:
+    num_1_float = float(numero_1)
+    num_2_float = float(numero_2)
+
+  except:
+    numeros_validos = None
+
+
+  if numeros_validos is None:
+    print('Um ou ambos os numeros digitados são invalidos')
+    continue
+
+  operadores_permitidos = '+-/*'
+
+  if operador not in operadores_permitidos:
+    print('Operador Invalido')
+    continue
+
+  if len(operador) > 1:
+    print('Digite apenas um operador')
+    continue
+
+  sair = input('Quer sair?  [s]im : ').lower().startswith('s')
+  
+  if sair is True:
+    break
+
+  print(sair)
