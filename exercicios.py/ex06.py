c39992273924d02d44c92720fb329412054d1743
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-12 17:19:25 -0300

    Create ex06.py

---
+++ exercicios.py/ex06.py
@@ -0,0 +1,14 @@
+""""calculadora com while
+lower()# converte em minuscula
+startswith('s')#começa com (s)
+"""
+while True:
+  print('nummmmm')
+
+
+  sair = input('Quer sair?  [s]im : ').lower().startswith('s')
+  
+  if sair is True:
+    break
+
+  print(sair)
