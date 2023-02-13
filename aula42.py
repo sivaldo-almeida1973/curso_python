1f84f8d2daa5d47477d4a5c0681d68f11d599d1c
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-13 16:42:54 -0300

    Update aula42.py

--- aula42.py
+++ aula42.py
@@ -1,37 +1,45 @@
-texto = 'Python s2'
+#texto = 'Python s2'
 
-i = 0
-tamanho_string = len(texto)
+#i = 0
+#t#amanho_string = len(texto)
 
-while i < tamanho_string:
-  print(texto[i], i)
-
-
-  i += 1
-
-print('-' *30)
-
-senha_salva = '123456'
-senha_digitada = ''
-repetições = 0
+#while i < tamanho_string:
+ # print(texto[i], i)
 
 
-while senha_salva != senha_digitada:
-  senha_digitada = input(f'sua senha ({repetições}X): ')
-
-  repetições += 1
-
-print(repetições)
-print('Aquele laço acima pode ter repetições infinitas')
+ # i += 1
 
 
-print('-' *30)
+  ########
+
+
+#print('-' *30)
+
+#senha_salva = '123456'
+#senha_digitada = ''
+#repetições = 0
+
+
+#while senha_salva != senha_digitada:
+#  senha_digitada = input(f'sua senha ({repetições}X): ')
+
+# repetições += 1
+
+#print(repetições)
+#print('Aquele laço acima pode ter repetições infinitas')
+
+#####
+#print('-' *30)
 #Introdução ao for(para)/in-estrutura de repetições para coisas finitas
 
-texto = 'python'
+texto = 'python' #iteravel
 
+novo_texto = ''
+for letra in texto:  #para cada letra em iteravel(texto) vou fazer uma 
 
-for letra in texto:
-  print(letra, letra * 10)
+  novo_texto += f'*{letra}'
+  print(letra) #iteração
+
+print(novo_texto  + '*')
 
   
