3af9356fc95728447fd3e62c6598320f82312404
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-16 18:53:53 -0300

    Create aula50.py

---
+++ aula50.py
@@ -0,0 +1,31 @@
+"""
+Cuidado com dados mútaveis
+= - copiado o valor(imutaveis)
+= - aponta para o mesmo valor na memoria(mutavel)
+"""
+
+nome = "sivaldo"
+noutra_variavel = nome
+nome = 'Alice'
+print(nome)
+print(noutra_variavel)
+
+
+print('='*30)
+#dessa forma não estamos copiando a lista
+lista_a = ['alice', 'lucas']
+lista_b = lista_a
+print(lista_b)
+lista_a[0] = 'sivaldo'
+print(lista_b)
+
+
+
+print('='*30)
+#dessa forma sim,  estamos copiando a lista usando (copy())
+lista_a = ['Alice', 'Lucas','Sivaldo']
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Guti'
+print(lista_a)
+print(lista_b)
