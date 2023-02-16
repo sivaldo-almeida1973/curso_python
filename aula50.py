f5acb18b8f479fdc6d3293810689dcd3c1e49d73
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-16 19:10:39 -0300

    Update aula50.py

--- aula50.py
+++ aula50.py
@@ -22,10 +22,27 @@ print(lista_b)
 
 
 print('='*30)
-#dessa forma sim,  estamos copiando a lista usando (copy())
+#dessa forma sim,  estamos copiando a lista usando (copy(
+# ))
 lista_a = ['Alice', 'Lucas','Sivaldo']
 lista_b = lista_a.copy()
 
 lista_a[0] = 'Guti'
 print(lista_a)
 print(lista_b)
+
+
+print('='*30)
+#For  in com listas
+
+lista = ['Maria','Lucas','Sivaldo','Alice']
+
+for nome in lista:
+  print(nome)
+
+
+
+print('='*30)
+
+for letra in 'ABC':
+  print(letra)
