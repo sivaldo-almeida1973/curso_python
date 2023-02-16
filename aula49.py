020ec0bce95cf242d94b6826fb9aa24887d670d5
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-16 18:53:47 -0300

    Create aula49.py

---
+++ aula49.py
@@ -0,0 +1,21 @@
+#Listas 
+#extend  
+#+ concatenar
+
+lista_a = [1,2,3,4,5]
+lista_b = ['a','b','c']
+lista_c = lista_a + lista_b  #união a + b
+
+print(lista_c)
+
+
+
+print('='*30)
+lista_a = [1,2,3,4,5]
+lista_b = ['a','b','c']
+lista_c = lista_a + lista_b  #união a + b
+lista_a.append(lista_b)  #coloca lista B dentro da lista A
+
+print(lista_a)
+
+
