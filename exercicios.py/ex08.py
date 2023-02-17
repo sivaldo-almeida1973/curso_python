3fec455236a5429cbc908097a316109b9dd5467f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-17 09:50:06 -0300

    Create ex08.py

---
+++ exercicios.py/ex08.py
@@ -0,0 +1,20 @@
+#Exiba os indices da lista
+
+lista = ['Maria','Sivaldo','Alice','Lucas']
+
+
+indices = range(len(lista))
+
+for indice in indices:
+  print(indice, lista[indice])
+
+
+print('='*30)
+
+lista = ['Maria','Alice','Lucas','Sivaldo','Guti','Antonio','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria','Maria']
+
+indices = range(len(lista))
+
+
+for indice in indices:
+  print(indice, lista[indice])
