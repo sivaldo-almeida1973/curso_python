7afcb9957666da023de9b2156a8e8b94d56bd3a2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-26 20:26:15 -0300

    Create aula059.py

---
+++ aula059.py
@@ -0,0 +1,13 @@
+"""
+Operação ternaria (condicional de uma linha) <valor> if <condicao> else <outro valor>
+"""
+#condicao =10 ==11
+#variavel = 'valor' if condicao else 'outro valor'
+#print(variavel)
+
+#digito = 10 # > 9 = 0
+#novo_digito = digito if digito <= 9 else  0
+#novo_digito = 0 if digito > 9 else digito
+#print(novo_digito)
+
+print('valor' if False else 'Outro valor' if False else 'fim')
