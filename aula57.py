185db8a9a57898d7c7f332a4fb2dd31704119407
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-26 17:47:01 -0300

    Create aula57.py

---
+++ aula57.py
@@ -0,0 +1,13 @@
+"""
+Interpretador do Python
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (inteativo com mod)
+
+the Zen of Python , por Tim Peters
+bonito é melhor que feio.
+explicito é melhor que implicito
+......
+"""
